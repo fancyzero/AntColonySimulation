287788ad60b3a598506bdd36681885c13cbbc41a
lizhou <lizhou@riotgames.com>
2021-03-30 01:17:49 +0800

    first

---
+++ Assets/Agent.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+    [System.Serializable]
+public class Agent : MonoBehaviour
+{  
+    public Spawner spawner;
+    public float speed = 1;
+    public float wonderingStrength = 0;
+    public float foodSearchRadius = 4;
+
+    float nextTimeToAddTrace = 0;
+    public float traceInterval = 0.2f;
+    Vector2 movingDirection = new Vector3(0,0);
+
+    Food food;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    Vector2 makeRandomDirection(Vector2 startWith)
+    {
+        return Random.insideUnitCircle ;
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (spawner == null)
+            return;
+        if ( food == null)
+        {
+            movingDirection = (movingDirection +  Random.insideUnitCircle*wonderingStrength);
+            movingDirection.Normalize();
+
+            
+            SearchFood();
+
+            var rot = Quaternion.LookRotation(new Vector3(movingDirection.x, 0,movingDirection.y) );
+            var p = this.GetComponent<Transform>().position;
+
+            if (Time.fixedTime > nextTimeToAddTrace)
+            {
+                
+                var tp = Instantiate(spawner.traceMark,  p,Quaternion.identity);
+                spawner.searchTrace.AddPoint(p,tp);
+                nextTimeToAddTrace =traceInterval+ nextTimeToAddTrace;
+            }            
+            p = p + new Vector3(movingDirection.x, 0,movingDirection.y)*Time.fixedDeltaTime*speed;
+
+            if (p.x > 50 || p.x < - 50)
+                movingDirection.x *=-1;
+            if (p.z > 50 || p.z < - 50)
+                movingDirection.y *= -1;
+
+            this.GetComponent<Transform>().SetPositionAndRotation( p, rot);
+
+            var minFood = GetClosestFood();
+            if (minFood != null)
+            {
+                var minDist = (minFood.GetComponent<Transform>().position -  transform.position).magnitude;
+
+                if ( minDist < 0.01f)
+                {
+                    food = minFood;
+                    Food.allFoods.Remove(food);
+                }
+            }
+
+
+
+        }
+            if (food != null)
+            {
+                food.transform.position = transform.position + transform.forward*1.0f;
+            }        
+    }
+
+    Food GetClosestFood()
+    {
+        Food minFood = null;
+        float minDist = 1000000;
+        foreach ( var f in Food.allFoods)
+        {
+            if ((f.GetComponent<Transform>().position - transform.position).magnitude < minDist)
+            {
+                minFood = f;
+                minDist = (f.GetComponent<Transform>().position - transform.position).magnitude;
+            }
+        }
+        return minFood;        
+    }
+
+    void SearchFood()
+    {
+        Food minFood = GetClosestFood();
+        if (minFood == null)
+            return;
+        var foodDir = (minFood.GetComponent<Transform>().position -  transform.position);
+        if (minFood != null && foodDir.magnitude < foodSearchRadius)
+        {
+            var v = foodDir.normalized;
+            movingDirection =new Vector2 (v.x, v.z);
+        }
+
+
+    }
+
+}
---
+++ Assets/TraceMap.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TracePoint
+{
+    public TracePoint(Vector2 p, float l,GameObject go)
+    {
+        position = p;
+        life=l;
+        gameObject = go;
+    }
+    public Vector2 position;
+    public float life;
+    public GameObject gameObject;
+}
+public class Cell
+{
+    public Cell()
+    {
+        points = new List<TracePoint>();
+    }
+    public List<TracePoint> points;
+}
+
+public class TraceMap 
+{
+    List<Cell> cells;
+    Vector2 tl;
+    Vector2 rb;
+
+    int hCount;
+    int vCount;
+
+    int size;
+    public TraceMap(int size,Vector2 tl, Vector2 rb)
+    {
+        this.tl = tl;
+        this.rb = rb;
+        this.size = size;
+        cells = new List<Cell>();
+        for (int i =0; i < size*size; i++) 
+        {
+            cells.Add( new Cell());
+        }
+    }
+    public void AddPoint(Vector2 pos, GameObject go)
+    {
+        var cellSize = (rb.x - tl.x)/size;
+        pos -= tl;
+        pos /=cellSize; 
+        int x = (int)(pos.x);
+        int y = (int)(pos.y);
+        cells[x+y*size].points.Add(new TracePoint(pos, 1,go));
+    }
+    public List<Vector2> GetPoints( Vector2 pos)
+    {
+        return null;
+    }
+
+    void Clean()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        foreach( var c in cells )
+        {
+            c.points.ForEach( x => x.life -= Time.fixedDeltaTime);
+            c.points.ForEach( x => if () {Destroy(x.gameObject)});
+            c.points.RemoveAll((x) => { return x.life <= 0; });
+        }
+    }
+}
